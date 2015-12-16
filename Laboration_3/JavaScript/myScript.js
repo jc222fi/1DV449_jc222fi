a1799b30fcaeb079803ab86312903ab66c3b0b3b
jc222fi <jc222fi@student.lnu.se>
2015-12-16 01:56:16 +0100

    trying something new

--- Laboration_3/JavaScript/myScript.js
+++ Laboration_3/JavaScript/myScript.js
@@ -1,9 +1,12 @@
 "use strict";
-var ApiResponse = {
-    init: function() {
+function ApiResponse(){
+    var allMessages = [];
+    var visibleMessages = [];
+
+    var getData = function(){
         var messages;
-        var marker = [];
         var map;
+        var marker = [];
 
         map = L.map('map').setView([63.045, 14.326], 5);
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -14,24 +17,31 @@ var ApiResponse = {
         }).addTo(map);
 
         $.getJSON("http://api.sr.se/api/v2/traffic/messages?format=json&size=50", function(data){
-
             for(var i in data.messages){
                 var date = parseInt(data.messages[i].createddate.substr(6));
                 data.messages[i].createddate = new Date(date);
             }
             //Sort messages based on dates, most recent one first, before saving them locally in messages
             messages = data.messages;
-            messages.sort(ApiResponse.sortOnDate);
+            messages.sort(sortOnDate);
 
             for(var i in data.messages) {
                 marker[i] = L.marker([data.messages[i].latitude, data.messages[i].longitude]).addTo(map);
-                ApiResponse.handleResponse(data.messages[i], map, marker[i]);
+                handleApiResponse(data.messages[i], map, marker[i]);
             }
         });
+        allMessages = messages;
+    };
+    this.initialize = function() {
+        var marker = [];
+
+        getData();
+        //console.log(getData());
+
         $("#output").on("click", "tr", function(){
-            for(var i in messages){
-                if(messages[i].id.toString() === $(this).context.id){
-                    ApiResponse.showDetails(messages[i], marker[i]).openPopup();
+            for(var i in allMessages){
+                if(allMessages[i].id.toString() === $(this).context.id){
+                    showDetails(allMessages[i], marker[i]).openPopup();
                 }
             }
         });
@@ -55,28 +65,25 @@ var ApiResponse = {
             $(".event").addClass("goldfish");
             $(".3").removeClass("goldfish");
         });
-    },
-    handleResponse: function(data, map, marker){
-        var html;
-        var date;
-
-        date = ApiResponse.formatDate(data.createddate);
-
-        //Draw the marker on the map
-        ApiResponse.showDetails(data, marker);
-
-        html = '<tr class="event ' + data.category + '" id="' + data.id + '"><td class="priority-' + data.priority + '">' + data.priority + '</td><td>' + data.subcategory + '</td><td>' + data.title + '</td><td class="date">' + date + '</td></tr>';
-        $('#output').append(html);
-    },
-    showDetails: function (data, marker){
-        return marker.bindPopup("<h4>" + data.title + "</h4><p>" + ApiResponse.formatDate(data.createddate) + '\n' + ApiResponse.formatTime(data.createddate) + '\n' + data.description + "</p>");
-    },
-    sortOnDate: function (a,b){
+    };
+    var sortOnDate = function(a, b){
         var dateA = new Date(a.createddate).getTime();
         var dateB = new Date(b.createddate).getTime();
         return dateA > dateB ? -1 : 1;
-    },
-    formatDate: function(date){
+    };
+    var handleApiResponse = function(data, map, marker) {
+        var html;
+        var date;
+
+        date = formatDate(data.createddate);
+
+        //Draw the marker on the map
+        showDetails(data, marker);
+
+        html = '<tr class="event ' + data.category + '" id="' + data.id + '"><td class="priority-' + data.priority + '">' + data.priority + '</td><td>' + data.subcategory + '</td><td>' + data.title + '</td><td class="date">' + date + '</td></tr>';
+        $('#output').append(html);
+    };
+    var formatDate = function(date) {
         var monthFormat;
         var dateFormat;
         var newDate;
@@ -96,8 +103,8 @@ var ApiResponse = {
         }
         newDate = date.getFullYear() + '-' + monthFormat + '-' + dateFormat;
         return newDate;
-    },
-    formatTime: function(date){
+    };
+    var formatTime = function(date) {
         var hours;
         var minutes;
 
@@ -114,7 +121,13 @@ var ApiResponse = {
             minutes = date.getMinutes();
         }
         return hours + ':' + minutes;
-    }
-};
+    };
+    var showDetails = function(data, marker) {
+        return marker.bindPopup("<h4>" + data.title + "</h4><p>" + formatDate(data.createddate) + '\n' + formatTime(data.createddate) + '\n' + data.description + "</p>");
+    };
+}
 
-window.onload = ApiResponse.init;
+$(document).ready(function(){
+    var apiResponseObj = new ApiResponse();
+    apiResponseObj.initialize();
+});
